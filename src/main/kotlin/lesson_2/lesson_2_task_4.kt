bd41e9f030681464eee7420adfcf8b3043fa59ed
Alexandr <alexandrsemc@gmail.com>
2023-10-02 22:22:24 +0300

    KS-2-4 the solution of the problem

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main(){
+    val crystalOre = 7
+    val ironOre = 11
+    val crystalOrePlus = crystalOre*(20/100)
+    val ironOrePlus = ironOre*(20/100)
+
+    println(crystalOrePlus)
+    println(ironOrePlus)
+
+}
