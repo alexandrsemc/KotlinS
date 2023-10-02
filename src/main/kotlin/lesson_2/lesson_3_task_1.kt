459f8c7fab14ff5e5ac00448c08fbd3dbf0b7263
Alexandr <alexandrsemc@gmail.com>
2023-10-02 22:54:18 +0300

    KS-3-1 the solution of the problem

---
+++ src/main/kotlin/lesson_2/lesson_3_task_1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+    var greetings = "Good afternoon"
+    val userName = "Star-Lord"
+
+    println("$greetings, $userName")
+
+    greetings = "Good evening"
+
+    println("$greetings, $userName")
+
+}
