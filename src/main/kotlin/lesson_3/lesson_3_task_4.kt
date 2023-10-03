7a88123f75c88bc763d78a306481e93a85833e4b
Alexandr <alexandrsemc@gmail.com>
2023-10-03 22:04:13 +0300

    KS-3-4 the solution of the problem

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,18 @@
+package lesson_3
+
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var moveNumber = 1
+    var progressData = "$from, $to, $moveNumber "
+
+    println(progressData)
+
+    from = "D2"
+    to = "D3"
+    moveNumber = 2
+    progressData = "$from, $to, $moveNumber "
+
+    println(progressData)
+
+}
