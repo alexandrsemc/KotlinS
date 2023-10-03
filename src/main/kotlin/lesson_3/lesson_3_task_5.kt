df1a8116e9a7d210cd685285609151154081233e
Alexandr <alexandrsemc@gmail.com>
2023-10-03 22:16:25 +0300

    KS-3-5 the solution of the problem

---
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main(){
+    val move = "D2-D4;0"
+    move.split('-',';')
+
+    val from = "D2"
+    val to = "D4"
+    val moveNumber = "0"
+
+    println("${from}\n${to}\n${moveNumber}")
+
+}
