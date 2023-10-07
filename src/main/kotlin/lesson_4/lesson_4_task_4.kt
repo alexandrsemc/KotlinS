19fc7f750c35185f50753268ee4150ba4aa11094
Alexandr <alexandrsemc@gmail.com>
2023-10-08 02:21:18 +0300

    KS-4-4 the solution of the problem (revision)

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -2,18 +2,18 @@ package lesson_4
 
 fun main(){
     val armExercise = "Упражнения для рук:"
-    val legExercise = "Упражнения для рук:"
-    val backExercise = "Упражнения для рук:"
-    val abdominalExercise = "Упражнения для рук:"
+    val legExercise = "Упражнения для ног:"
+    val backExercise = "Упражнения для спины:"
+    val abdominalExercise = "Упражнения для пресса:"
     val day = 5
-    val fairDay = ((day % 2 != 0))
-    val notAnEvenDay = ((day % 2 == 0))
+    val evenDay = ((day % 2 == 0))
 
     val conclusion  = """
-        ${armExercise}       ${fairDay}
-        ${legExercise}       ${notAnEvenDay}
-        ${backExercise}       ${notAnEvenDay}
-        ${abdominalExercise}       ${fairDay}
+        ${armExercise}       ${!evenDay}
+        ${legExercise}       ${evenDay}
+        ${backExercise}     ${evenDay}
+        ${abdominalExercise}    ${!evenDay}
     """.trimIndent()
+
     println(conclusion)
 }
