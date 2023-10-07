9339b11ecc9ddaa16949b464f7de53874693e85c
Alexandr <alexandrsemc@gmail.com>
2023-10-08 01:30:17 +0300

    KS-4-3 the solution of the problem (revision)

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,15 +1,15 @@
 package lesson_4
 
 fun main() {
-    val sunnyToday = true
-    val awningOpenToday = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumidityToday = 20
     val seasonToday = "winter"
-    val comparisonResult = (sunnyToday == SUNNY) && (awningOpenToday == AWNING_OPEN) && (airHumidityToday == AIR_HUMIDITY ) && (seasonToday == SEASON)
-    println("Благоприятные ли условия сейчас для роста бобовых? ${comparisonResult}")
+    val isComparisonResult = (isSunny == IS_SUNNY) && (isAwningOpen == IS_AWNING_OPEN) && (airHumidityToday == AIR_HUMIDITY ) && (seasonToday == SEASON)
+    println("Благоприятные ли условия сейчас для роста бобовых? ${isComparisonResult}")
 }
 
-const val SUNNY = true
-const val AWNING_OPEN = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
 const val SEASON = "not winter"
