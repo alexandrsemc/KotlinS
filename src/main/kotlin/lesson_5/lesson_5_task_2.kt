eddb6c79592a2515bc87406246b645807c7a9677
Alexandr <alexandrsemc@gmail.com>
2023-10-08 20:07:06 +0300

    KS-5-2 the solution of the problem

---
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_5
+
+fun main(){
+    val yearOfBirth = readln()!!.toInt()
+    val yearNow = 2023
+    val year = yearNow - yearOfBirth
+    val result = if (year >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+    else "Вернуться на главный экран"
+    println(result)
+}
+
+const val AGE_OF_MAJORITY = 18
