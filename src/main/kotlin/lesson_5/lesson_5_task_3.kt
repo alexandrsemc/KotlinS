9dd350f2c4964907eb7d2c98ccda5fab78a4ea71
Alexandr <alexandrsemc@gmail.com>
2023-10-08 20:30:52 +0300

    KS-5-3 the solution of the problem

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,20 @@
+package lesson_5
+
+fun main(){
+    println("Введите два числа от 1 до 100")
+    println("Введите первое число: ")
+    val yourNumber1 = readln()!!.toInt()
+    println("Введите второе число: ")
+    val yourNumber2 = readln()!!.toInt()
+    val result = if( (yourNumber1 == SUPER_NUMBER1 && yourNumber2 == SUPER_NUMBER2) || (yourNumber1 == SUPER_NUMBER2 && yourNumber2 == SUPER_NUMBER1))
+        "Поздравляем! Вы выйграли главный приз!"
+    else if ( (yourNumber1 == SUPER_NUMBER1 && yourNumber2 != SUPER_NUMBER2) || (yourNumber1 == SUPER_NUMBER2 && yourNumber2 != SUPER_NUMBER1))
+        "Вы выйграли утешительный приз"
+    else "Неудача! Крутите барабан"
+    println(result)
+    println("Победные числа: 7 и 21")
+
+}
+
+const val SUPER_NUMBER1 = 7
+const val SUPER_NUMBER2 = 21
