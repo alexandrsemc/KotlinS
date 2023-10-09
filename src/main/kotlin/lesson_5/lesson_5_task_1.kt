5bec9735bc386b9eded5b28aedf6895561690de9
Alexandr <alexandrsemc@gmail.com>
2023-10-09 23:07:45 +0300

    KS-5-1 the solution of the problem (revision)

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -2,7 +2,7 @@ package lesson_5
 
 fun main(){
     val numberFirst = 1
-    val numberSecond = 6
+    val numberSecond = 7
     val task  = numberFirst + numberSecond
 
     println("Решите пример и введите ответ: ${numberFirst} + ${numberSecond} ")
