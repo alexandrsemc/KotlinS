ca4d3501d599b3cec35e59025e0c74e4a19a5f48
Alexandr <alexandrsemc@gmail.com>
2023-10-10 23:40:19 +0300

    KS-5-3 the solution of the problem (revision)

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -8,11 +8,11 @@ fun main(){
     val yourNumber2 = readln().toInt()
     val result = if( (yourNumber1 == SUPER_NUMBER1 && yourNumber2 == SUPER_NUMBER2) || (yourNumber1 == SUPER_NUMBER2 && yourNumber2 == SUPER_NUMBER1))
         "Поздравляем! Вы выйграли главный приз!"
-    else if ( yourNumber1 == SUPER_NUMBER1  || yourNumber2 == SUPER_NUMBER2 )
+    else if (( yourNumber1 == SUPER_NUMBER1  || yourNumber2 == SUPER_NUMBER2 ) || (yourNumber1 == SUPER_NUMBER2 || yourNumber2 == SUPER_NUMBER1))
         "Вы выйграли утешительный приз"
     else "Неудача! Крутите барабан"
     println(result)
-    println("Победные числа: ${SUPER_NUMBER1} и ${SUPER_NUMBER2}")
+    println("Победные числа: $SUPER_NUMBER1 и $SUPER_NUMBER2")
 
 }
 
