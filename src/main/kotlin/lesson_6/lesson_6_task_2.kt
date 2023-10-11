1bbcb5c5cb965d644458dae54355c49b365a0ff0
Alexandr <alexandrsemc@gmail.com>
2023-10-11 23:21:28 +0300

    KS-6-2 the solution of the problem (revision)

--- src/main/kotlin/lesson_6/lesson_6_task_2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_2.kt
@@ -2,6 +2,7 @@ package lesson_6
 
 fun main() {
     println("Сколько засечь секунд?")
+
     val numberOfSeconds = readln().toInt()
     val millisecond = 1000
     val time = numberOfSeconds*millisecond
