298f9c5d079c468a28e74e68f014b7c7ac37a251
Alexandr <alexandrsemc@gmail.com>
2023-10-12 23:12:32 +0300

    KS-7-1 the solution of the problem (revision)

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -1,14 +1,15 @@
 package lesson_7
 
-fun main(){
+fun main() {
     val range1 = 0..9
     val range2 = 'a'..'z'
-    val listOfSymbols = range1 + range2
-    var counter = 6
+    var counter = 3
 
-    while (counter >0) {
+    while (counter > 0) {
         counter--
-        val result = listOfSymbols.random()
-        print(result)
+        val result1 = range1.random()
+        print(result1)
+        val result2 = range2.random()
+        print(result2)
     }
 }
