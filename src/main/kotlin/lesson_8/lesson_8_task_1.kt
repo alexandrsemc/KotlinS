5478ca48643c8ab613c67e1a8d25dbd6179182ff
Alexandr <alexandrsemc@gmail.com>
2023-10-14 23:21:19 +0300

    KS-8-2 the solution of the problem (revision)

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,7 +1,7 @@
 package lesson_8
 
 fun main() {
-
+    var sum = 0
     val day1 = readln().toInt()
     val day2 = readln().toInt()
     val day3 = readln().toInt()
@@ -10,6 +10,6 @@ fun main() {
     val day6 = readln().toInt()
     val day7 = readln().toInt()
     val viewsPerWeek = intArrayOf(day1, day2, day3, day4, day5, day6, day7)
-    println(viewsPerWeek.sum())
-
+    for (i in viewsPerWeek) sum += viewsPerWeek[i]
+    println(sum)
 }
