724c3d7c94b288dcab22bfb1378c925a3d238dd6
Alexandr <alexandrsemc@gmail.com>
2023-10-17 01:45:28 +0300

    KS-8-1 the solution of the problem (revision)

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,16 +1,11 @@
 package lesson_8
 
 fun main() {
+    val viewsPerWeek = IntArray(7)
 
-    var sum = 0
-    val day1 = readln().toInt()
-    val day2 = readln().toInt()
-    val day3 = readln().toInt()
-    val day4 = readln().toInt()
-    val day5 = readln().toInt()
-    val day6 = readln().toInt()
-    val day7 = readln().toInt()
-    val viewsPerWeek = intArrayOf(day1, day2, day3, day4, day5, day6, day7)
-    for (i in viewsPerWeek) sum += i
-    println(sum)
+    for (i in 0 until 7) {
+        println("Введите просмотры для ${i + 1} дня:")
+        viewsPerWeek[i] = readln().toInt()
+    }
+    println(viewsPerWeek.sum())
 }
