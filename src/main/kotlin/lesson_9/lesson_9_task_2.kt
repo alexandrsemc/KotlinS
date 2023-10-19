f5bcdb19cee8ffd524f31e8eb0896dd986d5019c
Alexandr <alexandrsemc@gmail.com>
2023-10-19 23:34:13 +0300

    KS-9-2 the solution of the problem (revision)

--- src/main/kotlin/lesson_9/lesson_9_task_2.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -2,19 +2,17 @@ package lesson_9
 
 fun main() {
     val list = mutableListOf("tea leaves", "boiling water", "piece of lemon")
-
     println("В рецепте есть базовые ингредиенты: $list")
     println("Желаете добавить еще?")
-
-    val ingredient = readln()
-    val yes = "yes"
-    if (yes == ingredient) {
-        println("Какой ингредиент вы хотели бы доавить?")
+    val userResponse = readln()
+    if (AGREEMENT_YES == userResponse) {
+        println("Какой ингредиент вы хотели бы добавить?")
         val newIngredient = readln()
         list.add(0, newIngredient)
     } else {
         return
     }
-
     println("Теперь в рецепте есть следующие ингредиенты: $list")
 }
+
+const val AGREEMENT_YES = "yes"
