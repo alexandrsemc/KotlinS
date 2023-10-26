dc4fdde44614a2cd652d029f7fbd3c889109ad8c
Alexandr <alexandrsemc@gmail.com>
2023-10-26 22:41:34 +0300

    KS-10-3 the solution of the problem (revision)

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -2,14 +2,11 @@ package lesson_10
 
 fun main() {
     val length = readln().toInt()
-    println(toGeneratePassword(length))
+    println(generatePassword(length))
 }
 
-fun toGeneratePassword(length: Int): String {
-    val symbols = listOf(
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!',
-        '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' '
-    )
+fun generatePassword(length: Int): String {
+    val symbols = ('0'..'9') + (' '..'/')
     return (1..length)
         .map { symbols.random() }
         .joinToString("")
