b34722549981bc6380d9cd4a096480f424ad4df8
Alexandr <alexandrsemc@gmail.com>
2023-10-28 18:25:07 +0300

    KS-10-2 the solution of the problem (revision)

--- src/main/kotlin/lesson_10/lesson_10_task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_2.kt
@@ -3,12 +3,14 @@ package lesson_10
 fun main() {
     val login = readln()
     val password = readln()
-    val result = if (getLength(login) && getLength(password)
-    ) "Добро пожаловать"
-    else "Логин и пароль недостаточно длиные"
+    val result =
+        if (validateLength(login) && validateLength(password))
+            "Добро пожаловать"
+        else
+            "Логин и пароль недостаточно длиные"
     println(result)
 }
 
-fun getLength(a: String): Boolean {
+fun validateLength(a: String): Boolean {
     return a.length >= 4
 }
