80b4b5bfbebb1dcaa297ef8ffcbae71d907d56e0
Alexandr <alexandrsemc@gmail.com>
2023-10-28 22:14:55 +0300

    KS-9-5 the solution of the problem (revision)

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -3,19 +3,9 @@ package lesson_9
 import java.util.*
 
 fun main() {
-
-    //  val userIngredients = "olives, cucumbers, potato, leek, water"
-    val ingredient1 = readln()
-    val ingredient2 = readln()
-    val ingredient3 = readln()
-    val ingredient4 = readln()
-    val ingredient5 = readln()
-    val ingredients = mutableListOf(ingredient1, ingredient2, ingredient3, ingredient4, ingredient5)
-    val sortedList = ingredients.sorted()
-    val convertToString = sortedList.joinToString()
+    val ingredients = MutableList(5) { readln() }.sorted().joinToString()
     val capitalizedString =
-        convertToString.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-    val list: List<String> = listOf(*capitalizedString.split(",").toTypedArray())
+        ingredients.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+    val list = setOf<String>(capitalizedString).joinToString()
     println(list)
-
 }
