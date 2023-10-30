726961266cec2e2cddb79a2ba9534f7d2acf1482
Alexandr <alexandrsemc@gmail.com>
2023-10-30 06:20:37 +0300

    KS-9-5 the solution of the problem (revision)

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -3,9 +3,8 @@ package lesson_9
 import java.util.*
 
 fun main() {
-    val ingredients = MutableList(5) { readln() }.sorted().joinToString()
+    val ingredients = MutableList(5) { readln() }.toSet().sorted().joinToString()
     val capitalizedString =
         ingredients.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-    val list = setOf<String>(capitalizedString).joinToString()
-    println(list)
+    println(capitalizedString)
 }
