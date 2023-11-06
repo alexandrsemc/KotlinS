5a2e04a2e7a6cfbe5ed64e9078d181e240d4ac5a
Alexandr <alexandrsemc@gmail.com>
2023-11-06 07:33:54 +0300

    KS-11-3 the solution of the problem (revision)

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -4,42 +4,30 @@ fun main() {
     Room(
         cover = "Fox",
         name = "Катя",
-        speakerStatus = listOf("разговаривает", "микрафон выключен", "пользователь заглушин"),
+        listOfParticipants = mutableListOf(),
     )
-
-    Member(
-        listOfParticipants = mutableListOf("Александр"),
-    )
-
     User5(
         cover = "Mouse",
         name = "Mickey",
-    )
-
-    Room(
-        cover = "Sun",
-        name = "Александр",
-        speakerStatus = listOf("разговаривает"),
+        speakerStatus = listOf ("разговаривает"),
     )
 }
-
-class Member(
-    val listOfParticipants: MutableList<String>,
-)
 
 class User5(
     val cover: String,
     val name: String,
+    val speakerStatus: List<String>,
 )
 
 class Room(
     val cover: String,
     val name: String,
-    val speakerStatus: List<String>,
+    val listOfParticipants: MutableList<User5>,
 ) {
     fun clickOnAvatar() {
 
     }
+
     fun microphoneAction() {
 
     }
