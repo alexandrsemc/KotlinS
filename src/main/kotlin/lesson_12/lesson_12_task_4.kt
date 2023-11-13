cd7b3bf186c58cc0e5d96200713c71c4df02fc38
Alexandr <alexandrsemc@gmail.com>
2023-11-13 20:02:24 +0300

    KS-12-4 the solution of the problem (revision)

--- src/main/kotlin/lesson_12/lesson_12_task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -1,14 +1,16 @@
 package lesson_12
 
 fun main() {
-    val weatherInCelsius = WeatherAtNoon(305, 297, false)
-    println("Погода:\nТемпература днем: ${weatherInCelsius.daytimeTemperature}\nТемпература ночью: ${weatherInCelsius.nightTemperature}\nОсадки: ${weatherInCelsius.precipitation}")
+    WeatherAtNoon(305, 297, false)
 }
 
 class WeatherAtNoon(temperatureInKelvinsDay: Int, temperatureInKelvinsNight: Int, rain: Boolean) {
     val daytimeTemperature = temperatureInKelvinsDay - TEMPERATURE_KELVINS
     val nightTemperature = temperatureInKelvinsNight - TEMPERATURE_KELVINS
     val precipitation = rain
+    init {
+        println("Погода:\nТемпература днем: ${daytimeTemperature}\nТемпература ночью: ${nightTemperature}\nОсадки: $precipitation")
+    }
 }
 
 const val TEMPERATURE_KELVINS = 273
