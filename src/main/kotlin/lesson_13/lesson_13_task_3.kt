5769ae05bf7125d9ff28fc6ee521f983cdc7b31e
Alexandr <alexandrsemc@gmail.com>
2023-11-15 17:36:52 +0300

    KS-13-3 the solution of the problem (revision)

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -1,20 +1,22 @@
 package lesson_13
 
 class PhoneBook(
-    var name: List<String>,
-    val phoneNumber: List<Long>,
-    var company: List<String?>? = null,
+    var name: String = "Ben",
+    val phoneNumber: Long = 84993333333,
+    var company: String? = null
 )
 
 fun main() {
-    val representative = PhoneBook(
-        name = listOf("Sam", "Ben", "Maksim", "Liza", "Karl"),
-        phoneNumber = listOf(84994444444, 84957777777, 85005555555, 86008888888, 93003333333),
-        company = listOf("Reddit", "Mars", "null", null, null),
+
+    val representative = listOf(
+        PhoneBook("Sam", 8799555555, "Subway"),
+        PhoneBook("Gregory", 8799555555),
+        PhoneBook("Maria", 8799555555, "Null"),
+        PhoneBook("Ksenia", 8798555555),
+        PhoneBook("Ben", 8799555555, "Sunlight"),
     )
-    println("-Name: ${representative.name[0]}\n-Number: ${representative.phoneNumber[0]}\n-Company: ${representative.company?.get(0)}\n")
-    println("-Name: ${representative.name[1]}\n-Number: ${representative.phoneNumber[1]}\n-Company: ${representative.company?.get(1)}\n")
-    println("-Name: ${representative.name[2]}\n-Number: ${representative.phoneNumber[2]}\n-Company: ${representative.company?.get(2)}\n")
-    println("-Name: ${representative.name[3]}\n-Number: ${representative.phoneNumber[3]}\n-Company: ${representative.company?.get(3)}\n")
-    println("-Name: ${representative.name[4]}\n-Number: ${representative.phoneNumber[4]}\n-Company: ${representative.company?.get(4)}\n")
+
+    val listOfCompanies = representative.map { it -> it.company }
+        .toList()
+    println(listOfCompanies)
 }
