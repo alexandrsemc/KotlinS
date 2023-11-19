949bb559e3dce7b7ada307213c07098b24409964
Alexandr <alexandrsemc@gmail.com>
2023-11-19 07:41:57 +0300

    KS-16-1 the solution of the problem

---
+++ src/main/kotlin/lesson_16/lesson_16_task_1.kt
@@ -0,0 +1,16 @@
+package lesson_16
+
+class Cube() {
+    private val randomNumber = 1
+
+    fun numberPrintout() {
+        println(randomNumber)
+    }
+}
+
+fun main() {
+
+    val cube = Cube()
+    cube.numberPrintout()
+
+}
