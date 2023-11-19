5e094b2c143a59e37f2cc3a8f75a27a2e4bc3edf
Alexandr <alexandrsemc@gmail.com>
2023-11-19 13:16:15 +0300

    KS-10-3 the solution of the problem (revision)

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -4,16 +4,17 @@ fun main() {
     val length = readln().toInt()
     println(generatePassword(length))
 }
+
 fun generatePassword(length: Int): String {
     val numbers = ('0'..'9')
     val symbols = (' '..'/')
-    var n = 1
-    while (n <= length / 2) {
-        val result1 = numbers.random()
-        print(result1)
-        val result2 = symbols.random()
-        print(result2)
-        n += 1
+    var password = ""
+    for (i in 1..length / 2) {
+        password += numbers.random()
+        password += symbols.random()
     }
-    return length.toString()
+    if (length % 2 != 0) {
+        password += numbers.random()
+    }
+    return password
 }
